5d87637d7885332d7d8243cafe022a9e05e7f2d2
anhtruong <phanhpompap@gmail.com>
2023-11-01 15:07:47 +0100

    plantlist ready

--- client/src/pages/Plantspage.jsx
+++ client/src/pages/Plantspage.jsx
@@ -5,24 +5,25 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const PlantsPage = () => {
-  //   const [plants, setPlants] = useState([]);
-  //   const [isLoading, setIsLoading] = useState(true);
+  const [plants, setPlants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  //   useEffect(() => {
-  //     axios
-  //       .get("https://greendiary-server.onrender.com/plants")
-  //       .then((response) => {
-  //         setPlants(response.data);
-  //         setIsLoading(false);
-  //         console.log(response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error:", error);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    axios
+      .get("https://greendiary-server.onrender.com/plants")
+      .then((response) => {
+        setPlants(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-  //   if (isLoading) {
-  //     return <p>Loading</p>;
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
   return (
     <div id="plants-container">
       <div id="plants-search">
@@ -30,30 +31,12 @@ const PlantsPage = () => {
         <button>Search By Image</button>
       </div>
       <div className="grid-container" id="plants-list">
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
-        <div className="grid-item">
-          <img src={imageExample}></img>
-          <p>plants name</p>
-        </div>
+        {plants.map((plant) => (
+          <div className="grid-item" key={plant.id}>
+            <img src={plant.url}></img>
+            <p>{plant.name}</p>
+          </div>
+        ))}
       </div>
       <div id="plants-pagination">
         <button>More</button>
