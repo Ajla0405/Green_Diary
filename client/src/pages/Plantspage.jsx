681e3ddb4de6312d260a890b18a227a541e79cdd
anhtruong <phanhpompap@gmail.com>
2023-11-01 14:20:11 +0100

    hardcode

--- client/src/pages/Plantspage.jsx
+++ client/src/pages/Plantspage.jsx
@@ -1,22 +1,53 @@
 import React from "react";
 import "./PlantsPage.css";
+import imageExample from "../images/heroImage4.jpg";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const PlantsPage = () => {
+  const [plants, setPlants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://greendiary-server.onrender.com/plants")
+      .then((response) => {
+        setPlants(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
   return (
     <div id="plants-container">
       <div id="plants-search">
         <button>Search By Name</button>
         <button>Search By Image</button>
       </div>
-      <div className="grid-container" id="plants-list">
-        <div className="gird-item">1</div>
-        <div className="gird-item">2</div>
-        <div className="gird-item">3</div>
-        <div className="gird-item">4</div>
-        <div className="gird-item">5</div>
-        <div className="gird-item">6</div>
+      {plants.map((plant) => {
+        <div className="grid-item">
+          <img src={plant.url}></img>
+          <p>Name of plant</p>
+        </div>;
+      })}
+      ;
+      {plants.map((plant) => {
+        <div className="grid-container" id="plants-list">
+          <div className="grid-item">
+            <img src={imageExample}></img>
+            <p>{plants.name}</p>
+          </div>
+        </div>;
+      })}
+      <div id="plants-pagination">
+        <button>More</button>
       </div>
-      <div id="plants-pagination"></div>
     </div>
   );
 };
